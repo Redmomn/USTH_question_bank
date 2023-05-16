05004424d39290fd30e4d8f033a2e04367938bf5
Redmomn <109732988+Redmomn@users.noreply.github.com>
2023-05-16 21:32:15 +0800

    fix.

---
+++ 新思想概论（2023年）/analysis.py
@@ -0,0 +1,44 @@
+import json
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# 原始json整理
+def raw_json(filename):
+    with open(file=filename,mode='r',encoding='utf-8') as f:
+        a = json.load(fp=f) # 去除多余反斜杠
+    b = a['text']
+    b = json.loads(b) #去多余引号
+    return b['BusinessData']['BC_xcqm_tkb_SC']
+
+def sw_dict_to_list(filename):
+    with open(file=filename,mode='r',encoding='utf-8') as f:
+        a = json.load(fp=f)
+    return a['BusinessData']['BC_xcqm_tkb_SC']
+
+def xxzh(i):
+    t_list=[]
+    try:
+        for j in range(1,10):
+            te:str = i[f'xx{j}']
+            if te == '':
+                continue
+            if te.startswith(i['da']):
+                t_list.append(f'    - [x] {te}\n')
+            else:
+                t_list.append(f'    - [ ] {te}\n')
+    except Exception:
+        pass
+    return t_list
+
+title_list = raw_json('a1.json')
+mdfile = '单选.md'
+try:
+    f = open(mdfile,mode='r',encoding='utf-8')
+    f.close()
+except Exception:
+    with open(mdfile,mode='w',encoding='utf-8'):
+        pass
+with open(mdfile,mode='a',encoding='utf-8') as f:
+    for i in title_list:
+        f.write(f"{i['xh']}. {i['wt']}\n")
+        f.writelines(xxzh(i))
---
+++ 新思想概论（2023年）/test.py
@@ -0,0 +1,19 @@
+import json
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with open(file='a1.json',mode='r',encoding='utf-8') as f:
+    a = json.load(fp=f) #去反斜杠
+# print(type(a))
+# b = a.replace("\\","")
+
+b = a['text']
+b = json.loads(b) #去多余引号
+
+print(len(b['BusinessData']['BC_xcqm_tkb_SC']))
+# b = json.loads(str(a))
+
+with open(file='a2.json',mode='w',encoding='utf-8') as f:
+    json.dump(fp=f,obj=b,ensure_ascii=False,indent=4,separators=(',',':'))
+# print(b)
+print(type(b))
