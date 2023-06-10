2e2ce7ddd1d167e9a373887980e74f6f16760c88
Redmomn <9601742+Redmonm@user.noreply.gitee.com>
2023-06-10 12:51:55 +0800

    fix.增加对word文档的支持

--- 马克思主义基本原理概论/test.py
+++ 马克思主义基本原理概论/test.py
@@ -2,7 +2,7 @@ import json
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-with open(file='test.json',mode='r',encoding='utf-8') as f:
+with open(file='判断.json',mode='r',encoding='utf-8') as f:
     a = json.load(fp=f) #去反斜杠
 # print(type(a))
 # b = a.replace("\\","")
@@ -13,7 +13,7 @@ a['text'] = b
 # print(len(b['BusinessData']['BC_xcqm_tkb_TF']))
 # b = json.loads(str(a))
 
-with open(file='test2.json',mode='w',encoding='utf-8') as f:
-    json.dump(fp=f,obj=a,ensure_ascii=False,indent=4,separators=(',',':'))
+with open(file='3.json',mode='w',encoding='utf-8') as f:
+    json.dump(fp=f,obj=b["BusinessData"],ensure_ascii=False,indent=4,separators=(',',':'))
 # print(b)
 print(type(b))
